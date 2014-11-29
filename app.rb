79385dd16da9e2cdc5fb6384d5a4d3fee2be59d4
CHEN Qing <qing.chen@rea-group.com>
2014-11-29 17:39:44 +0800

    add inputting reminder message

--- app.rb
+++ app.rb
@@ -6,27 +6,32 @@ require 'json'
 require_relative  'env.rb'
 
 on_text do
+  text = params[:Content]
+  result = []
+  if %w(1 2).include? text
     # parsed_json = (HTTParty.get("http://www.kidsmeet.cn?name=#{params[:FromUserName]}&eventType=all")).parsed_response
-  file = File.read('events.json')
-  parsed_json = JSON.parse(file) #mocked results
-    result = []
+    file = File.read('events.json')
+    parsed_json = JSON.parse(file) #mocked results
     parsed_json.map do |item|
       result << {
           :title => item['title'],
           :description => item['abstract'],
           :picture_url => item['main_image_url'],
-          :url => URI.encode("#{SETTINGS[:app_url]}/events/#{item['id']}")
+          :url => URI.encode("#{SETTINGS[:app_url]}/events/1")
       }
     end
+  else
+    result = '查阅活动内容请发送“1”最新热门活动,“2”经典回顾'
+  end
   result
 end
 
 on_subscribe do
-    "感谢您的订阅"
+  '感谢您的订阅!查阅活动内容请发送“1”最新热门活动,“2”经典回顾'
 end
 
 on_unsubscribe do
-    "欢迎您再次订阅"
+    "欢迎您再次订阅!"
 end
 
 token 'server_access_token'
